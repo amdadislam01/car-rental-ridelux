427074211d3e6f3d086f44fd675e676c1e430d45
amdadislam01 <amdadislam733@gmail.com>
2025-09-11 20:22:41 +0600

    Banner car update

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css';
 import { Pagination, Autoplay } from 'swiper/modules'
 
 import about from '../assets/about.jpg';
 
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import carctg1 from '../assets/car-ctg-01.png';
+import carctg2 from '../assets/car-ctg-02.png';
+import carctg3 from '../assets/car-ctg-03.png';
+import carctg4 from '../assets/car-ctg-04.png';
+
 const Index = () => {
+
+  const [pickUpDate, setPickUpDate] = useState(null);
+  const datePickerRef = useRef(null)
+
+  const openCalender = () => {
+    if (datePickerRef.current) {
+      datePickerRef.current.setFocus();
+    }
+  };
+
+  const [returnDate, setReturnDate] = useState(null);
+  const returnPickerRef = useRef(null);
+
+  const openreturnCalender = () => {
+    if (returnPickerRef.current) {
+      returnPickerRef.current.setFocus();
+    }
+  }
+
   return (
     <>
       {/* Hero Section */}
@@ -101,6 +128,42 @@ const Index = () => {
             <button className='bg-[#e8021f] text-white px-8 py-4 rounded-full font-medium flex items-center gap-2 hover:bg-black transition-colors duration-300 cursor-pointer'>
               Read More <i className="ri-arrow-right-line"></i>
             </button>
+          </div>
+        </div>
+      </div>
+      {/* Banner Section */}
+      <div className="banner lg:px-[12%] px-[8%] py-[50px] lg:py-[90px]">
+        <div className="banner-content text-center">
+          <p className="uppercase text-sm tracking-[5px] text-white mb-2">- Rent Now</p>
+          <h2 className="text-4xl md:text-5xl font-bold mb-3 text-white bricolage-font">Book Auto Rental</h2>
+          <div className="bg-[#1f1f1f] text-white w-[90%] max-w-[1200px] mx-auto mt-[70px] rounded-3xl px-6 py-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 shadow-lg z-50">
+
+            {/* Cars Type*/}
+            <div className="relative w-full lg:w-auto px-4 py-3 group border-r border-gray-600">
+              <button className='flex items-center gap-2 w-full justify-between text-gray-400'>Choose Car Type <i className="ri-arrow-down-s-line text-[#e8021f]"></i></button>
+              <div className="absolute top-[110%] left-0 w-48 bg-[#1f1f1f] border border-[#e8021f] rounded-sm shadow-md opacity-0 scale-95 invisible group-hover:opacity-100 group-hover:scale-100 group-hover:visible transition-all duration-300 ease-in-out z-50">
+                <ul className='divide-y divide-gray-700'>
+                  <li className='px-4 py-2 hover:bg-[#e8021f] transition cursor-pointer'>Choose Car Type</li>
+                  <li className='px-4 py-2 hover:bg-[#e8021f] transition cursor-pointer'>Sports Cars</li>
+                  <li className='px-4 py-2 hover:bg-[#e8021f] transition cursor-pointer'>Convertible</li>
+                  <li className='px-4 py-2 hover:bg-[#e8021f] transition cursor-pointer'>Luxury Cars</li>
+                  <li className='px-4 py-2 hover:bg-[#e8021f] transition cursor-pointer'>Small Cars</li>
+                </ul>
+              </div>
+            </div>
+
+            {/* Pick Location */}
+            <div className="relative w-full lg:w-auto px-4 py-3 group border-r border-gray-600">
+              <button className='flex items-center gap-2 w-full justify-between text-gray-400'>Choose Car Location <i className="ri-arrow-down-s-line text-[#e8021f]"></i></button>
+              <div className="absolute top-[110%] left-0 w-48 bg-[#1f1f1f] border border-[#e8021f] rounded-sm shadow-md opacity-0 scale-95 invisible group-hover:opacity-100 group-hover:scale-100 group-hover:visible transition-all duration-300 ease-in-out z-50">
+                <ul className='divide-y divide-gray-700'>
+                  <li className='px-4 py-2 hover:bg-[#e8021f] transition cursor-pointer'>Japan</li>
+                  <li className='px-4 py-2 hover:bg-[#e8021f] transition cursor-pointer'>New York</li>
+                  <li className='px-4 py-2 hover:bg-[#e8021f] transition cursor-pointer'>Bangladesh</li>
+                  <li className='px-4 py-2 hover:bg-[#e8021f] transition cursor-pointer'>Dubai</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
