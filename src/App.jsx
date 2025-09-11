41745166b68dd372c51e52f171e7be56ab72341f
amdadislam01 <amdadislam733@gmail.com>
2025-09-11 15:14:21 +0600

    car-rental-ridelux project setup complate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='text-4xl text-center font-bold'>Hlw I am Amdad Islam</h1>
+    </>
+  )
+}
+
+export default App
